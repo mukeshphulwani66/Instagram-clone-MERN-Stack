1ea74a5ae22aebc2baa285b8696e6899d520f4f2
mukesh phulwani <mukeshphulwani55@gmail.com>
2020-04-16 00:22:57 +0530

    cloudinary

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -39,7 +39,7 @@ const CretePost = ()=>{
    const postDetails = ()=>{
        const data = new FormData()
        data.append("file",image)
-       data.append("upload_preset","insta-clone")
+       data.append("upload_preset","new-insta")
        data.append("cloud_name","cnq")
        fetch("https://api.cloudinary.com/v1_1/cnq/image/upload",{
            method:"post",
